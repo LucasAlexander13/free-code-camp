90ece69251881754ff962bd7ce17409d81a42808
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-20 19:17:04 -0300

    Error treatment simplified

--- scientific-computing-with-python/boilerplate-arithmetic-formatter/arithmetic_arranger.py
+++ scientific-computing-with-python/boilerplate-arithmetic-formatter/arithmetic_arranger.py
@@ -2,19 +2,15 @@ def arithmetic_arranger(problems, result = False):
 
     if len(problems) > 5:
         return "Error: Too many problems."
-
+    
     for problem in problems:
         if "x" in problem or "/" in problem:
             return "Error: Operator must be '+' or '-'."
     
-    for problem in problems:
         for value in problem:
             if value.isalnum():
                 return "Error: Numbers must only contain digits."
-
-    for problem in problems:
-        for value in problem:
-            if len(value) > 4:
+            elif len(value) > 4:
                 return "Error: Numbers cannot be more than four digits."
 
 
