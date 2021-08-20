983cfbfd9f8a6cb6aa915462660d04f989a05f2d
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-20 18:52:16 -0300

    Added too many problems error

--- scientific-computing-with-python/boilerplate-arithmetic-formatter/arithmetic_arranger.py
+++ scientific-computing-with-python/boilerplate-arithmetic-formatter/arithmetic_arranger.py
@@ -1,4 +1,10 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, result = False):
+
+    if len(problems) > 5:
+        raise "Error: Too many problems."
 
 
     return arranged_problems
+
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
