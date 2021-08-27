4870f45408e9f666c31eb335a3225653be4557e1
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-27 20:43:39 -0300

    Calculate new hour and minutes

--- scientific-computing-with-python/boilerplate-time-calculator/time_calculator.py
+++ scientific-computing-with-python/boilerplate-time-calculator/time_calculator.py
@@ -3,7 +3,14 @@ def add_time(start, duration, day=None):
     start_min = int(start[-5:-3])
     period = start[-2:]
 
-    duration_hrs = duration[:-3]
-    duration_min = duration[-2:]
+    duration_hrs = int(duration[:-3])
+    duration_min = int(duration[-2:])
+
+    new_min = start_min + duration_min
+    if new_min >= 60:
+        new_min -= 60
+        duration_hrs += 1
+    
+    new_hrs = start_hrs + duration_hrs
 
     return new_time
