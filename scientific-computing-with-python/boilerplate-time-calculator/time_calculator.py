434e70be7ccff3d8115d05594830790bad627dcc
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-27 20:39:02 -0300

    Added time variables

--- scientific-computing-with-python/boilerplate-time-calculator/time_calculator.py
+++ scientific-computing-with-python/boilerplate-time-calculator/time_calculator.py
@@ -1,7 +1,9 @@
-def add_time(start, duration):
+def add_time(start, duration, day=None):
+    start_hrs = int(start[:-6])
+    start_min = int(start[-5:-3])
+    period = start[-2:]
 
-
-
-
+    duration_hrs = duration[:-3]
+    duration_min = duration[-2:]
 
     return new_time
