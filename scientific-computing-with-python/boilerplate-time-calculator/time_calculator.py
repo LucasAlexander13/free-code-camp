c6daae135932958ad9d2ca8459d7bac6fc544a5d
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-27 21:14:05 -0300

    Added a next day function to find what day is

--- scientific-computing-with-python/boilerplate-time-calculator/time_calculator.py
+++ scientific-computing-with-python/boilerplate-time-calculator/time_calculator.py
@@ -1,4 +1,14 @@
-def add_time(start, duration, day=None):
+def next_day(day, days_gone):
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    position = days.index(day)
+
+    position += days_gone
+    while position > 6:
+        position - 7
+    
+    return days[position]
+
+def add_time(start, duration, actual_day=None):
     start_hrs = int(start[:-6])
     start_min = int(start[-5:-3])
     period = start[-2:]
@@ -12,12 +22,12 @@ def add_time(start, duration, day=None):
         duration_hrs += 1
     
     new_hrs = start_hrs + duration_hrs
-    days = 0
+    days_gone = 0
 
     if period == "AM":
         while new_hrs >= 24:
             new_hrs -= 24
-            days += 1
+            days_gone += 1
         if new_hrs >= 13:
             new_hrs -= 12
             period = "PM"
@@ -25,17 +35,20 @@ def add_time(start, duration, day=None):
     elif period == "PM":
         while new_hrs >= 24:
             new_hrs -= 24
-            days += 1
+            days_gone += 1
         if new_hrs >= 13:
             new_hrs -= 12
-            days += 1
+            days_gone += 1
             period = "AM"
     
     pass_days = ""
-    if days == 1:
+    if days_gone == 1:
         pass_days = "(next day)"
-    elif days > 1:
-        pass_days = f"{days} days later"
+    elif days_gone > 1:
+        pass_days = f"{days_gone} days later"
     
-
-    return new_time
+    if actual_day != None:
+        actual_day = actual_day.title()
+        new_day = next_day(actual_day, days_gone)
+        return new_time
+    else:
