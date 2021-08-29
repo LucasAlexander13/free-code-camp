f31f5e692bbf0e2b137ed10e69f85bee84eefa40
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-29 18:08:17 -0300

    Added rectangle attributes and some methods

--- scientific-computing-with-python/boilerplate-polygon-area-calculator/shape_calculator.py
+++ scientific-computing-with-python/boilerplate-polygon-area-calculator/shape_calculator.py
@@ -2,11 +2,19 @@ from _typeshed import Self
 
 
 class Rectangle:
-    def __init__(self):
-        pass
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
+    def set_width(self, width):
+        self.width = width
 
+    def set_height(self, height):
+        self.height = height
 
+    def get_area(self):
+        return self.width * self.width
+    
 
 class Square(Rectangle):
     def __init__(self):
