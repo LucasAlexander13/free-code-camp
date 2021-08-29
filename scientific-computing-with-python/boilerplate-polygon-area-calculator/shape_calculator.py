cd6a8bf36eaa8949c3e71b9e55a9015242824381
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-29 18:17:20 -0300

    Added get_picture method

--- scientific-computing-with-python/boilerplate-polygon-area-calculator/shape_calculator.py
+++ scientific-computing-with-python/boilerplate-polygon-area-calculator/shape_calculator.py
@@ -22,8 +22,17 @@ class Rectangle:
     def get_diagonal(self):
         diagonal = (self.width ** 2 + self.height ** 2) ** 0.5
         return diagonal
-
     
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        else: picture = ""
+
+        for line in range(self.height):
+            picture += "*" * self.width + "\n"
+        
+        return picture
+
 
 class Square(Rectangle):
     def __init__(self):
