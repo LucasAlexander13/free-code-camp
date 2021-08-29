a55f4ed408620870a0f36ff847796009ec5b7128
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-29 18:03:29 -0300

    Initied budget project

--- scientific-computing-with-python/boilerplate-budget-app/budget.py
+++ scientific-computing-with-python/boilerplate-budget-app/budget.py
@@ -1,4 +1,11 @@
-class Category:
+from _typeshed import Self
+class Category():
+    def __init__(self) -> None:
+        self.ledger = []
+
+    def deposit(self, amount, description=""):
+        text = f"\"amount\": {amount}, \"description\": {description}"
+        self.ledger.append("{"+text+"}")
 
 
 
