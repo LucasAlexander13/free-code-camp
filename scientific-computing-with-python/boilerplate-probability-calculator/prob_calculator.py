1a039c1b327422a0be808ddc012e98f314784d2a
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-31 19:35:21 -0300

    Created hat class

--- scientific-computing-with-python/boilerplate-probability-calculator/prob_calculator.py
+++ scientific-computing-with-python/boilerplate-probability-calculator/prob_calculator.py
@@ -2,7 +2,13 @@ import copy
 import random
 # Consider using the modules imported above.
 
-class Hat:
+class Hat():
+    def __init__(self, **kwargs):
+        self.contents = []
+        for key, value in kwargs.items():
+            for i in range(value):
+                self.contents.append(key)
+
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
