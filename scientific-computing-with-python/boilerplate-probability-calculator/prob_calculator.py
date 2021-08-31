2a78760694d082764d42819a7275e0a8f26f5bbc
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-31 19:42:04 -0300

    Added draw method

--- scientific-computing-with-python/boilerplate-probability-calculator/prob_calculator.py
+++ scientific-computing-with-python/boilerplate-probability-calculator/prob_calculator.py
@@ -9,6 +9,15 @@ class Hat():
             for i in range(value):
                 self.contents.append(key)
 
-
+    def draw(self, number):
+        removed_list = []
+        for i in range(number):
+            removed = random.choice(self.contents)
+            removed_list.append(removed)
+            
+            index = self.contents.index(removed)
+            self.contents.pop(index)
+        
+        return removed_list
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
