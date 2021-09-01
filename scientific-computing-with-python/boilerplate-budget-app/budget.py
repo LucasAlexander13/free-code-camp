ae9fe859222cf615bc5b2ffd3ad8f7176807f351
Lucas Alexander <lucasalexander13@hotmail.com>
2021-09-01 17:47:57 -0300

    Added withdraw method

--- scientific-computing-with-python/boilerplate-budget-app/budget.py
+++ scientific-computing-with-python/boilerplate-budget-app/budget.py
@@ -3,11 +3,21 @@ class Category:
     def __init__(self, name):
         self.category = name
         self.ledger = []
+        self.amount = 0
 
     def deposit(self, amount, description=""):
         text = f"\"amount\": {amount}, \"description\": {description}"
         self.ledger.append("{"+text+"}")
+        self.amount += amount
 
+    def withdraw(self, amount, description=""):
+        if amount < self.amount:
+            text = f"\"amount\": -{amount}, \"description\": {description}"
+            self.ledger.append("{"+text+"}")
+            self.amount -= amount
+            return True
+        else:
+            return False
 
 
 
